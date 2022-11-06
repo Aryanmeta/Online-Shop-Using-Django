79f59282f53de80189e01711286b4f8a693676d9
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-06 11:22:36 +0330

    Create home page & about us page

---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
+    path('aboutus/', views.AboutPageView.as_view(), name='aboutus'),
+]
