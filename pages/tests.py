84478ceaef6036678b889aad1f5091fa1be2c6e4
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-06 12:13:53 +0330

    test for aboutus page

--- pages/tests.py
+++ pages/tests.py
@@ -18,3 +18,21 @@ class HomePageTest(TestCase):
     def test_home_page_template_used(self):
         response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'home.html')
+
+
+class AboutUSPageTest(TestCase):
+    def test_aboutus_page_url_by_name(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_aboutus_page_content(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertContains(response, 'About US Page')
+
+    def test_aboutus_page_url(self):
+        response = self.client.get('/aboutus/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_aboutus_page_template_used(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertTemplateUsed(response, 'pages/aboutus.html')
