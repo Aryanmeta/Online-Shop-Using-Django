6339dfe1a45591cbd1325ecb37c81cd2e8707dfe
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-08 20:59:22 +0330

    Create login page & signup page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,17 @@
-from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
+# from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        models = get_user_model()
+        # models = CustomUser
+        model = get_user_model()
         fields = ('email', 'username', )
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
+        # models = CustomUser
         model = get_user_model()
         fields = ('email', 'username',)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
-from . import views
+from .views import SignUpView
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
