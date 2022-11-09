723a3aab0b6b966b66ccf12ad9e525b2ad71ed80
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-09 13:04:45 +0330

    test for signup page

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class SignUpPageTest(TestCase):
+    username = 'username'
+    email = 'username@gmail.com'
+
+    def test_signup_page_by_url_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_by_url(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_template_used(self):
+        response = self.client.get(reverse('signup'))
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_signup_page_content(self):
+        response = self.client.get(reverse('signup'))
+        self.assertContains(response, 'Sign Up')
+
+    def test_signup_form(self):
+        user = get_user_model().objects.create_user(
+            self.username,
+            self.email
+        )
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
