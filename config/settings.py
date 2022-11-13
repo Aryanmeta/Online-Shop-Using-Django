3227eb2a1ef58bd457e3df9e94ff230017dce545
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-13 23:42:08 +0330

    Environment Variables for debug

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
 
 # Application definition
