f672be147769743e997db996701f384533304e29
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-15 14:40:19 +0330

    Product Detail View

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='product_list')
+    path('', ProductListView.as_view(), name='product_list'),
+    path('/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -8,3 +8,10 @@ class ProductListView(generic.ListView):
     queryset = Product.objects.filter(active=True)
     template_name = 'products/product_list.html'
     context_object_name = 'products'
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+
