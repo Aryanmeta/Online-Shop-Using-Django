f23047d6925b76435d18eeb85fd085471ad73869
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-15 14:39:46 +0330

    Update Product List View

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Product(models.Model):
@@ -12,3 +13,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.id])
