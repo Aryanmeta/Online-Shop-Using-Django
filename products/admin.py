30451b7b0d730f2237c0fffdd9265f5fdbb7b5e8
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-16 21:26:54 +0330

    Create Comment model

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', 'datetime_created']
+
+
+@admin.register(Comment)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product', 'author', 'starts', 'datetime_created', 'active']
--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class Product(models.Model):
@@ -16,3 +17,22 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.id])
+
+
+class Comment(models.Model):
+    PRODUCT_STARS = [
+        ('1', 'Very Bad'),
+        ('2', 'Bad'),
+        ('3', 'Normal'),
+        ('4', 'Good'),
+        ('5', 'Perfect'),
+    ]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments',)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments',)
+    body = models.TextField()
+    starts = models.CharField(max_length=10, choices=PRODUCT_STARS)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    active = models.BooleanField(default=True)
