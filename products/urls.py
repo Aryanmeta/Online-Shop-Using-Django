bcda8afa8cc657021a8f7dcc35b429d9f38b7260
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-17 23:26:56 +0330

    add url for comment create view

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, CommentCreateView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('comment/<int:product_id>/', CommentCreateView.as_view(), name='comment_create'),
 ]
