2dbe828511cf2c5e340e0de6a866f6c0122d4f6d
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-17 23:30:34 +0330

    Create Comment_Create_View for post a comment

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
-from .models import Product
+from .models import Product, Comment
 from .forms import CommentForm
 
 
@@ -20,3 +22,21 @@ class ProductDetailView(generic.DetailView):
         context = super().get_context_data(**kwargs)
         context['comment_form'] = CommentForm
         return context
+
+
+class CommentCreateView(generic.CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    # def get_success_url(self):
+    #     return reverse('product_list')
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+
+        product_id = int(self.kwargs['product_id'])
+        product = get_object_or_404(Product, id=product_id)
+        obj.product = product
+
+        return super().form_valid(form)
