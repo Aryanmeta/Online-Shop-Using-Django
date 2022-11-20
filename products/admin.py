b0de610a98505eafc5921764aba0738f38b0857c
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-20 12:28:51 +0330

    Implement Tbular In Line for comments

--- products/admin.py
+++ products/admin.py
@@ -3,11 +3,23 @@ from django.contrib import admin
 from .models import Product, Comment
 
 
+class CommentsInLine(admin.TabularInline):  # class CommentsInLine(admin.StackedInline):
+    model = Comment
+    # readonly_fields = ['author', 'body', 'starts', 'active', ]
+    fields = ['author', 'body', 'starts', 'active', ]
+    raw_id_fields = ['author', ]
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price', 'active', 'datetime_created']
+    list_display = ['title', 'price', 'active', 'datetime_created', ]
+
+    inlines = [
+        CommentsInLine,
+    ]
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['product', 'author', 'body', 'starts', 'datetime_created', 'active']
+    list_display = ['product', 'author', 'body', 'starts', 'datetime_created', 'active', ]
