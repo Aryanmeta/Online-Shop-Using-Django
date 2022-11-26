d18dc14272a7a30adbf9c6e4b2e532ab42d833a2
aryan zarei <aryan2020.zarei@gmail.com>
2022-11-26 17:30:36 +0330

    Add product to cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,5 +1,8 @@
 class Cart:
     def __init__(self, request):
+        """
+        Initialize the cart
+        """
         self.request = request
 
         self.session = request.session
@@ -10,3 +13,22 @@ class Cart:
             cart = self.session['cart'] = {}
 
         self.cart = cart
+
+    def add(self, product, quantity=1):
+        """
+        Add the specified product to the cart if it exists
+        """
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': quantity}
+        else:
+            self.cart[product_id]['quantity'] += quantity
+
+        self.save()
+
+    def save(self):
+        """
+        Mark session as modified to save changes
+        """
+        self.session.modified = True
