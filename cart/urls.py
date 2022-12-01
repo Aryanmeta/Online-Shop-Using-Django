9d7d9bb82409857670a75dc652ac42883dc20ffc
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-01 22:52:30 +0330

    add url for add_to_cart_view

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import cart_detail_view
+from .views import cart_detail_view, add_to_cart_view
 
 app_name = 'cart'
 
 urlpatterns = [
     path('', cart_detail_view, name='cart_detail'),
+    path('add/<int:product_id>/', add_to_cart_view, name='cart_add'),
 ]
