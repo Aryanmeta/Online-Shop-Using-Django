c9c205b3f06ad66eb05b41a9635e5eead9d26b81
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-01 13:24:41 +0330

    Add to cart view & update cart detail view cart/views.py

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from products.models import Product
+from .cart import Cart
+from .forms import AddToCartProductForm
 
 
 def cart_detail_view(request):
-    return render(request, 'cart/cart.detail.html', )
+    cart = Cart(request)
+
+    return render(request, 'cart/cart_detail.html', {
+        'cart': cart,
+    })
+
+
+def add_to_cart_view(request, product_id):
+    cart = Cart(request)
+
+    product = get_object_or_404(Product, id=product_id)
+    form = AddToCartProductForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        quantity = cleaned_data['quantity']
+        cart.add(product, quantity)
+
+    return redirect('cart:cart_detail')
