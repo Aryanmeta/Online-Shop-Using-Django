8718c0c42797594ab7bd259f4a79b2e5b555cab3
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-02 11:38:08 +0330

    Views for remove from cart

--- cart/views.py
+++ cart/views.py
@@ -25,3 +25,13 @@ def add_to_cart_view(request, product_id):
         cart.add(product, quantity)
 
     return redirect('cart:cart_detail')
+
+
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+
+    product = get_object_or_404(Product, id=product_id)
+
+    cart.remove(product)
+
+    return redirect('cart:cart_detail')
