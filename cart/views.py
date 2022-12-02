6303a20a57650985ede9531388e6d024c7326137
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-02 14:29:12 +0330

    Create product update quantity form

--- cart/views.py
+++ cart/views.py
@@ -8,6 +8,12 @@ from .forms import AddToCartProductForm
 def cart_detail_view(request):
     cart = Cart(request)
 
+    for item in cart:
+        item['product_update_quantity_form'] = AddToCartProductForm(initial={
+            'quantity': item['quantity'],
+            'inplace': True,
+        })
+
     return render(request, 'cart/cart_detail.html', {
         'cart': cart,
     })
@@ -22,7 +28,7 @@ def add_to_cart_view(request, product_id):
     if form.is_valid():
         cleaned_data = form.cleaned_data
         quantity = cleaned_data['quantity']
-        cart.add(product, quantity)
+        cart.add(product, quantity, replace_current_quantity=cleaned_data['inplace'])
 
     return redirect('cart:cart_detail')
 
