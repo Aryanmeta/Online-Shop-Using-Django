7eac9cb185055bcd841b6e45e09b6d84afe19e66
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-03 11:52:20 +0330

    require_POST for add to cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 
 from products.models import Product
 from .cart import Cart
@@ -19,6 +20,7 @@ def cart_detail_view(request):
     })
 
 
+@require_POST
 def add_to_cart_view(request, product_id):
     cart = Cart(request)
 
