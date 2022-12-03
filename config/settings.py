f2ce3bf3ca78af98baae1701cfd0384d424604ea
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-03 19:17:01 +0330

    Context processors

--- config/settings.py
+++ config/settings.py
@@ -83,6 +83,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # Custom Context Processors
+                'cart.context_processors.cart',
             ],
         },
     },
