04e7c610d053c7d99e3ae9267479d69b1b590cb7
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-03 19:15:14 +0330

    Upadte len cart/cart

--- cart/cart.py
+++ cart/cart.py
@@ -69,7 +69,7 @@ class Cart:
             yield item
 
     def __len__(self):
-        return len(self.cart.keys())
+        return sum(item['quantity'] for item in self.cart.values())
 
     def clear(self):
         del self.session['cart']
