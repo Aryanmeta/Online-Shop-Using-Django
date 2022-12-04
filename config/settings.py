09e1c7c24ec418e0bb2f24db3ee83b17e873b11b
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-04 20:47:10 +0330

    Install pillow package

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from django.conf.global_settings import MEDIA_ROOT
 from django.contrib.messages import constants as messages
 from pathlib import Path
 from environs import Env
@@ -155,6 +155,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
+# media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
