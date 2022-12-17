56abf17df565adee24eddf38805325ec6945e6b6
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-17 09:49:29 +0330

    Create persian_translate_tags for translate number template filter & Implement translate number templete filter [persian_translate]

---
+++ persian_translate/templatetags/persian_traslation_tags.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def translate_number(value):
+    value = str(value)
+    english_to_persian_table = value.maketrans('0123456789', '۰١٢٣٤٥٦٧٨٩')
+    return value.translate(english_to_persian_table)
+
