e7f6ee648b577c06ad976d3779f4b1b62c3b393c
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-22 18:15:33 +0330

    Create OrderAdmin & OrderAdmin [Orders]

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderItemInLine(admin.TabularInline):
+    model = OrderItem
+    fields = ['order', 'product', 'quantity', 'price', ]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'phone_number', 'address', 'order_note',
+                    'datetime_created', 'datetime_modified', 'is_paid', ]
+
+    inlines = [
+        OrderItemInLine,
+    ]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity', 'price', ]
