882bb277b51ff9bb07df4c1786f8b7c879e3b529
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-24 02:43:22 +0330

    Implement order_create_view for add order [orders]

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from .forms import OrderForm
 
 
+@login_required
 def order_create_view(request):
-    return render(request, 'orders/order_create.html', context={
-        'form': OrderForm()
+    order_form = OrderForm()
+
+    if request.method == 'POST':
+        order_form = OrderForm(request.POST, )
+
+        if order_form.is_valid():
+            order_obj = order_form.save(commit=False)
+            order_obj.user = request.user
+            order_obj.save()
+
+    return render(request, 'orders/order_create.html', {
+        'form': order_form,
     })
+
