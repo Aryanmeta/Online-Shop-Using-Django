bc8d4e1c0c87757d30f5457c0114d1b292d3963c
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-24 02:42:07 +0330

    Update order models.py [orders]

--- orders/models.py
+++ orders/models.py
@@ -8,17 +8,17 @@ class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_('User'))
     is_paid = models.BooleanField(_('Finalized ?'), default=False)
 
-    first_name = models.CharField(_('First Name'), max_length=50, blank=True)
-    last_name = models.CharField(_('Last Name'), max_length=50, blank=True)
+    first_name = models.CharField(_('First Name'), max_length=50, blank=False)
+    last_name = models.CharField(_('Last Name'), max_length=50, blank=False)
     # An optional phone number.
     # phone_number = PhoneNumberField(_('Phone Number'), blank=True)
-    phone_number = models.CharField(_('Phone Number'), max_length=15 , blank=True)
+    phone_number = models.CharField(_('Phone Number'), max_length=15, blank=False)
 
-    address = models.CharField(_('Address'), max_length=700)
+    address = models.CharField(_('Address'), max_length=700, blank=False)
     order_note = models.TextField(_('Note'), blank=True)
 
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    datetime_created = models.DateTimeField(_('Created'), auto_now_add=True)
+    datetime_modified = models.DateTimeField(_('Modified'), auto_now=True)
 
     def __str__(self):
         return f'Order : {self.id}'
