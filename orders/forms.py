edea12a2e70d367076019034b36aa2b2a2501e58
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-23 19:07:33 +0330

    Update i18n [orders]

--- orders/forms.py
+++ orders/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from .models import Order
 
@@ -9,5 +10,6 @@ class OrderForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'phone_number', 'address', 'order_note']
         widgets = {
             'address': forms.Textarea(attrs={'rows': 3}),
-            'order_note': forms.Textarea(attrs={'rows': 4, 'placeholder': 'If you have any note please type here: '}),
+            'order_note': forms.Textarea(attrs={'rows': 4,
+                                                'placeholder': _('If you have any note please type here: ')}),
         }
