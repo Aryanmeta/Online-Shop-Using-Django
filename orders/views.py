5514806d11795716955bd8cc91120150ac46798a
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-23 12:43:32 +0330

    Send context OrderForm in view [orders]

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .forms import OrderForm
+
 
 def order_create_view(request):
-    return render(request, 'orders/order_create.html')
+    return render(request, 'orders/order_create.html', context={
+        'form': OrderForm()
+    })
